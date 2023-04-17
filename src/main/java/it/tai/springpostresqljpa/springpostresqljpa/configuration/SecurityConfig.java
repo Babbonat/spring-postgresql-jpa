191dbaa628d7176f731b60e3370fbf23c0606f87
Babbonat <44708155+Babbonat@users.noreply.github.com>
2023-04-17 08:41:36 +0200

    iniziata config OAuth2

--- src/main/java/it/tai/springpostresqljpa/springpostresqljpa/configuration/SecurityConfig.java
+++ src/main/java/it/tai/springpostresqljpa/springpostresqljpa/configuration/SecurityConfig.java
@@ -1,15 +1,18 @@
 package it.tai.springpostresqljpa.springpostresqljpa.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
+@Slf4j
 public class SecurityConfig{ //extends WebSecurityConfigurerAdapter {
 
 	/*@Override
@@ -29,14 +32,21 @@ public class SecurityConfig{ //extends WebSecurityConfigurerAdapter {
 	}*/
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
 	{
 		http
-				.authorizeHttpRequests()
-				.anyRequest()
-				.authenticated()
-				.and()
-				.oauth2ResourceServer();
+				.authorizeHttpRequests((authorize) -> authorize
+						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/tutorials/{tutorialId}/comments", "/api/comments/{commentId}")
+						.hasAuthority("SCOPE_read")
+						.requestMatchers(HttpMethod.POST, "/api/tutorials/{tutorialId}/comments")
+						.hasAuthority("SCOPE_write")
+						.requestMatchers(HttpMethod.PUT, "/api/comments/{commentId}")
+						.hasAuthority("SCOPE_update")
+						.requestMatchers(HttpMethod.DELETE, "/api/comments/{commentId}", "/api/tutorials/{tutorialId}/comments")
+						.hasAuthority("SCOPE_delete").anyRequest().authenticated()
+				)
+				.oauth2ResourceServer().jwt();
 		return http.build();
 	}
 
